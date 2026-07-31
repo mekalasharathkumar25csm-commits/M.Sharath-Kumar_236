6cccb48ec6155c5c6d67b28199dcefa7fd0510ce
mekalasharathkumar25csm-commits <mekalasharathkumar.25.csm@anits.edu.in>
2026-07-31 12:13:17 +0530

    Add files via upload

---
+++ Hierarchical.java
@@ -0,0 +1,36 @@
+package inheritance;
+
+
+class Vehicle {
+ void start() {
+     System.out.println("Vehicle is starting");
+ }
+}
+
+
+class Car extends Vehicle {
+ void drive() {
+     System.out.println("Car is driving");
+ }
+}
+
+
+class Bike extends Vehicle {
+ void ride() {
+     System.out.println("Bike is riding");
+ }
+}
+
+
+public class Hierarchical {
+ public static void main(String[] args) {
+
+     Car c = new Car();
+     c.start();
+     c.drive();
+
+     Bike b = new Bike();
+     b.start();
+     b.ride();
+ }
+}
