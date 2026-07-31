13cc23ba7db20eb536c7e136dc88d877ba5b6ce9
mekalasharathkumar25csm-commits <mekalasharathkumar.25.csm@anits.edu.in>
2026-07-31 11:33:55 +0530

    Add files via upload

---
+++ Single.java
@@ -0,0 +1,24 @@
+package inheritance;
+
+class Animal {
+ void eat() {
+     System.out.println("Animal is eating");
+ }
+}
+
+
+class Dog extends Animal {
+ void bark() {
+     System.out.println("Dog is barking");
+ }
+}
+
+public class Single {
+ public static void main(String[] args) {
+     Dog d = new Dog();
+
+     d.eat();   
+     d.bark();  
+ }
+}
+
