da1bd7087fcff97da765f9f5986c1580d201063d
mekalasharathkumar25csm-commits <mekalasharathkumar.25.csm@anits.edu.in>
2026-08-07 17:58:07 +0530

    Update PartA.java

--- PartA.java
+++ PartA.java
@@ -5,7 +5,7 @@ public class PartA {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input
+        
         System.out.print("Enter Student Name: ");
         String name = sc.nextLine();
 
@@ -21,11 +21,11 @@ public class PartA {
         System.out.print("Enter Marks in Subject 3: ");
         int m3 = sc.nextInt();
 
-        // Calculation
+        
         int total = m1 + m2 + m3;
         double percentage = total / 3.0;
 
-        // Output
+        
         System.out.println("\n----- Student Information -----");
         System.out.println("Name       : " + name);
         System.out.println("Roll No    : " + rollNo);
