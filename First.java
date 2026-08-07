2700faece678c24e242eb3f11d2295543c925c34
mekalasharathkumar25csm-commits <mekalasharathkumar.25.csm@anits.edu.in>
2026-08-07 11:20:00 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,47 @@
+package interfaces;
+interface I1 {
+    int x = 7;
+    void sample();
+}
+
+interface I2 extends I1 {
+    int y = 8;
+    void demo();
+}
+
+class multi implements I1, I2 {
+
+    public void sample() {
+        System.out.println("this is from I1");
+    }
+
+    public void demo() {
+        System.out.println("this is from I2");
+    }
+
+    void mymethod() {
+        System.out.println("from child class");
+    }
+}
+
+public class First {
+    public static void main(String[] args) {
+
+        multi mlt = new multi();
+
+        System.out.println(I1.x);
+        System.out.println(I2.y);
+
+        mlt.demo();
+        mlt.sample();
+        mlt.mymethod();
+    }
+}
+    
+   
+
+
+
+
+
+
