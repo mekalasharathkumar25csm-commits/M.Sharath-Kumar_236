994f9b0e3811ce71085b64a6ef470b4030a4994c
mekalasharathkumar25csm-commits <mekalasharathkumar.25.csm@anits.edu.in>
2026-08-09 17:04:56 +0530

    Add files via upload

---
+++ Practice Programs/Constructor/ConDemoTwo.java
@@ -0,0 +1,26 @@
+
+public class ConDemoTwo {
+	int rollno;
+	String name;
+	float fee;
+	public ConDemoTwo(int i,String n,float f) {
+		this.rollno= i;
+		this.name=" n";
+		this.fee=f;
+		
+		
+	}
+	void sample() {
+		System.out.println("rollno is:" + this.rollno);
+		System.out.println("name is:" + this.name);
+		System.out.println("fee is:" + this.fee);
+	}
+		public static void main(String[] args) {
+			System.out.println("hello ");
+			 ConDemo cd= new ConDemo();
+			 cd.sample();
+		}
+			
+	}
+		
+
